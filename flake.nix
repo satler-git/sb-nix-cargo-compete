2d7a5c5bbd8a958e12c1d206debc9cab4a492795
satler <satler@satler.dev>
2025-01-25 12:11:53 +0900

    fix: use `~` instead of `$HOME`

--- flake.nix
+++ flake.nix
@@ -75,9 +75,9 @@
                     ]
                   )
                 } \
-                --run 'export PATH=$PATH:"$HOME/.cache/cargo-compete/rustup/bin"' \
-                --run 'export RUSTUP_HOME=''${RUSTUP_HOME-"$HOME/.cache/cargo-compete/rustup"}' \
-                --run 'export CARGO_HOME=''${CARGO_HOME-"$HOME/.cache/cargo-compete/rustup"}' \
+                --run 'export PATH=$PATH:~/.cache/cargo-compete/rustup/bin' \
+                --run 'export RUSTUP_HOME=''${RUSTUP_HOME-~/.cache/cargo-compete/rustup}' \
+                --run 'export CARGO_HOME=''${CARGO_HOME-~/.cache/cargo-compete/rustup}' \
                 --run 'rustup default stable'
             '';
           };
